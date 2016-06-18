e49c73ae6852ebc1c1a42a88c9ed3e99da6ca8a0
Kalior <kalioragus@gmail.com>
2016-06-18 16:01:54 +0200

    Modify play function to play the melody as in the html

--- js/instrument.js
+++ js/instrument.js
@@ -13,15 +13,31 @@ $(document).ready(function () {
 });
 
 $("#play-sound").click(function () {
-  gainSum = 0;
+  var tones = [261.63, 329.628, 391.995]
+  var melody = [200, 400, 100, 440, 220, 320]
+
+  // Setting tempo to 60 BPM just for now
+  var tempo = 120.0;
+  var secondsPerBeat = 60.0 / tempo;
+
+  for (var i = 0; i <= 7; i++) {
+    for (var j = 0; j < 3; j++) {
+      if ($("#melody-row-" + j + "-column-" + i).is(":checked")) {
+        playSound(tones[j], context.currentTime + 0.25 * i * secondsPerBeat, 0.25 * secondsPerBeat);
+      }
+    }
+  }
+});
+
+function playSound (freq, time, length) {
+  var gainSum = 0;
   for (var i = 0; i <= $("#number-overtones-input").val(); i++) {
     gainSum += $("#gain-control-" + i).val() / 100;
   }
 
   for (var i = 0; i <= $("#number-overtones-input").val(); i++) {
-    createOscilator(100 * (i+1), $("#gain-control-" + i).val() / (100 * gainSum));
+    createOscilator(freq * (i+1), $("#gain-control-" + i).val() / (100 * gainSum), time, length);
   }
-});
 }
 
 function round (value, decimals) {
