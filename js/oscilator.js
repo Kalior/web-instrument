62e3e275738f6ea519f3ffbdc41bfa172013f180
Kalior <kalioragus@gmail.com>
2016-07-31 18:03:16 +0200

    Bug fix, attack now works

--- js/oscilator.js
+++ js/oscilator.js
@@ -7,14 +7,14 @@ function createOscilator (freq, gain, startTime, length, attack, decay, sustain,
 
   // Setting the Envelope
   var gainNode = context.createGain();
-  gainNode.gain.setValueAtTime(0.0, 0);
+  gainNode.gain.setValueAtTime(0.0, startTime);
   // Attack
   gainNode.gain.linearRampToValueAtTime(gain, startTime + attack);
   // Decay
   gainNode.gain.setTargetAtTime(gain * envelopeSustainGain, startTime + attack, decay * 0.2);
   // Release
   var timeBeforeRelease = startTime + attack + decay;
-  if (release > 0) {
+  if (sustain > 0) {
     timeBeforeRelease += sustain;
   }
   gainNode.gain.setTargetAtTime(0.0, timeBeforeRelease, release * 0.2);
